2354e8b5cbe0e614cc6a5e66bbecabb33d522e74
Tommi Ojaskivi <Tommi@Tommis-MacBook-Pro.local>
2022-04-01 13:08:05 +0200

    done-ish

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,49 +2,36 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import hero from '../public/alster-symbol-white.png'
+import hero from '../public/hero.png'
+import mockup from '../public/splash.png'
+import mobile from '../public/mobile.png'
+import desktop from '../public/desktop.png'
+import footer from '../public/footer.png'
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Alsterverse - the place to be</title>
+        <title>Alsterverse™ - the place to be</title>
         <meta name="description" content="Alsterverse" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
 
-        <Image src={hero} alt="Welcome to Alsterverse"/>
-        
-        <h1 className={styles.title}>
-          Welcome to Alsterverse
-        </h1>
+      {hero && <img src={hero.src} className={styles.hero} alt="Welcome to Alsterverse" />}
 
-        <div>
-          Enter the universe
-        </div>
-        <div className={styles.grid}>
-          {/* <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>About &rarr;</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, quia?</p>
-          </a>
-
-          
-
-          <a href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}>
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a> */}
-        </div>
+      {mockup && <img src={mockup.src} className={styles.mockup} alt="" />}
+      <div className={styles.links}>
+      <a href="#">
+      {mobile && <img src={mobile.src} className={styles.mobile} alt="" />}
+      </a>
+      {desktop && <img src={desktop.src} className={styles.desktop} alt="" />}
+      </div>
+      {footer && <img src={footer.src} className={styles.footer} alt="" />}
+    
       </main>
 
-      <footer className={styles.footer}>
-        
-        </footer>
     </div>
   )
 }
